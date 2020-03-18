45154ead589524a63071c9ddc8addae8abdd78c6
otherr <den.batia@gmail.com>
2020-03-18 20:58:13 +0300

    граф с большего

--- 1.py
+++ 1.py
@@ -1,17 +1,69 @@
-import datetime
-from random import randint
-def sort(arri):
-    if len(arri) < 2:
-        return arri
-    else:
-        a = arri.pop(int(len(arri)/2))
-        min = [i for i in arri if i <= a]
-        max = [i for i in arri if i > a]
-        return sort(min) + [a] + sort(max)
+graf = {}
+count = {}
+count['s'] = 0
+count['B'] = float('inf')
+count['C'] = float('inf')
+count['D'] = float('inf')
+count['A'] = float('inf')
+count['f'] = float('inf')
+parent = {}
+list_s = []
 
-s = [randint(0,i) for i in range(500000)]
-print(len(s))
-t1 = datetime.datetime.now()
-s= sort(s)
-t2= datetime.datetime.now() - t1
-print('{} секунд, {} милиекунд'.format(t2.seconds, t2.microseconds/1000))
+graf['s'] = {}
+
+graf['s']['A'] = 350
+graf['s']['B'] = 200
+
+graf['A'] = {}
+graf['A']['C'] = 100
+graf['A']['D'] = 100
+
+graf['B'] = {}
+graf['B']['A'] = 100
+graf['B']['D'] = 150
+
+graf['C'] = {}
+graf['C']['D'] = 200
+graf['C']['f'] = 250
+
+graf['D'] = {}
+graf['D']['f'] = 100
+
+graf['f'] = {}
+
+def min_count(list):
+    a = float('inf')
+    b = None
+    for i in list:
+        if i in list_s:
+            continue
+        if list[i] < a:
+            a = list[i]
+            b = i
+    return b
+
+def serch():
+    a = min_count(count)
+
+    for i in graf[a]:
+        if i not in list_s:
+            q = graf[a][i] + count[a]
+            if q < count[i]:
+                count[i] = q
+    list_s.append(a)
+
+serch()
+print(count)
+serch()
+print(count)
+
+serch()
+print(count)
+
+serch()
+print(count)
+
+serch()
+print(count)
+
+print()
